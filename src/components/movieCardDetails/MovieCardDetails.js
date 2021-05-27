2ec383bc45467285e0126dbebd2f05f17f4739dd
bilelBZ <bilelbz155@gmail.com>
2021-05-27 12:47:32 +0100

    router

--- src/components/movieCardDetails/MovieCardDetails.js
+++ src/components/movieCardDetails/MovieCardDetails.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
-
-const MovieCardDetails = ({movielist,match}) => {
-    const movie = movielist.find((el)=>el.id == match.params.id)
+import React from 'react';
+import './styling.css'
+const MovieCardDetails = ({movieList,match}) => {
+    const movie = movieList.find((el)=>el.id == match.params.id)
     return (
-<div>
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={movie.posterUrl} />
-  <Card.Body>
-    <Card.Title>{movie.title}</Card.Title>
-    <Card.Text>
-      {movie.description}
-    </Card.Text>
-  </Card.Body>
-</Card>
-<div>
-    <h3>Trailer</h3>
-    <div>
-    {movie.trailer}
-    </div>
+<div className='cardDetails'>
+<div width='50%' >
+<h3>{movie.title}</h3>
+<img width='400' src={movie.posterUrl} alt={movie.title}/>
+<h6>{movie.description}</h6>
+</div>
+<div width='50%'>
+    <h3>Trailer</h3>   
+    <iframe
+     title="trailer" 
+      width='800'
+      height="400"
+       src={movie.trailer}
+       allowFullScreen
+       />
 </div>
 </div>
     )
